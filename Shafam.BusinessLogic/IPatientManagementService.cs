e4a200f2545734e5f2a3c7c97ac8068f78001109
arijkhan <arijkhan@outlook.com>
2014-11-17 18:24:24 -0500

    Added IPatientManagementService Facade

---
+++ Shafam.BusinessLogic/IPatientManagementService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shafam.Common.DataModel;
+
+namespace Shafam.BusinessLogic
+{
+    interface IPatientManagementService
+    {
+        Patient ViewPatient(int patientId);
+        List<Patient> ViewAllPatients(int doctorId);
+        int AddPatient(Patient patient, int doctorId);
+        bool UpdatePatient(Patient patient, int visitationId);
+        bool ReferPatient(int patientId, int referringDocId, int referredDocId);
+    }
+}
