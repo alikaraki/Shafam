f9ebff39f3f515f95a7130fc2398e83af2952381
Filza Mazahir <filza.mazahir@gmail.com>
2014-11-19 12:39:52 -0500

    Fixed visitation mgmt files & folder (empty)

--- Shafam.BusinessLogic/VisitationManagement/VisitationManagementService.cs
+++ Shafam.BusinessLogic/VisitationManagement/VisitationManagementService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shafam.BusinessLogic.Visitation_Management
+namespace Shafam.BusinessLogic.VisitationManagement
 {
     class VisitationManagementService
     {
