e67ef4e4622def746dd2444bb3d4450e27e62742
mojtabam <mmoodi@uwaterloo.ca>
2014-11-22 11:09:13 -0500

    Adding Billing and Legal interfaces with minor changes

--- Shafam.Common/DataModel/Staff.cs
+++ Shafam.Common/DataModel/Staff.cs
@@ -7,7 +7,6 @@
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-
         public string Department { get; set; }
     }
 }
