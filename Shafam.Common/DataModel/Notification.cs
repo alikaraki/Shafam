72d6f855616cfa71febb5a74c382329a3e37c360
arijkhan <arijkhan@outlook.com>
2014-11-24 03:00:33 -0500

    Added properties to Notification class

--- Shafam.Common/DataModel/Notification.cs
+++ Shafam.Common/DataModel/Notification.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Shafam.Common.DataModel
 {
-    class Notification
+    public class Notification
     {
+        public NotificationType Type { get; set; }
+        public string Message { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
     }
 }
